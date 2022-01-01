299cb103de2478e3d38a6d9a69cb2aa70f34320e
Ahmed Elsayes <mysterious.en2010@gmail.com>
2022-01-01 18:03:51 +0200

    UseEffect to access DOM attributes of Input element

--- react_practice/full-react-app/my-app/src/HookExamples/DifferentHooks/UseRefExplainer.tsx
+++ react_practice/full-react-app/my-app/src/HookExamples/DifferentHooks/UseRefExplainer.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { createRef, useEffect, useRef, useState } from 'react'
 
 export default function UseRefExplainer() {
   const [jobName, setJobName] = useState("")
   // const [counts, setCounts] = useState(1)
   const counts = useRef(1)
+  const inputRef = createRef<HTMLInputElement>()
   useEffect(() => {
     // setCounts(prevCount => prevCount+1)
-    counts.current = counts.current+1
+    counts.current = counts.current + 1
   }, [jobName])
   console.log(jobName, counts)
+
+  const activateFocuse = () => {
+    // console.log("Reference to input: ", inputRef.current)
+    inputRef.current?.focus()
+  }
   return (
     <>
       <hr />
@@ -17,13 +23,14 @@ export default function UseRefExplainer() {
         <div className='row justify-content-center align-items-center mt-5'>
           <div className='col-md-4 col-xs-12'>
             <input
+              ref={inputRef}
               value={jobName}
               onChange={e => setJobName(e.target.value)}
               placeholder='Enter your job name'
             />
           </div>
           <div className='col-md-4 col-xs-12'>
-            <button className='special-button'> Take me to input </button>
+            <button onClick={activateFocuse} className='special-button'> Take me to input </button>
           </div>
         </div>
         <div className='row justify-content-center align-items-center mt-5 mb-5'>
